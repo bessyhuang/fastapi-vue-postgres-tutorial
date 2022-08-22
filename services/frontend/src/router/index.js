d83be0cfce38b9e8c2808366a0994610430f9545
bessyhuang <bessyhuang1@gmail.com>
2022-08-23 01:17:33 +0800

    Build Views pages

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -5,6 +5,10 @@ import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue';
 import Register from '@/views/Register.vue';
 import Login from '@/views/Login.vue';
+import Dashboard from '@/views/Dashboard.vue';
+import Profile from '@/views/Profile';
+import Note from '@/views/Note';
+import EditNote from '@/views/EditNote';
 
 
 Vue.use(VueRouter)
@@ -24,6 +28,32 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {requiresAuth: true},
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+    meta: {requiresAuth: true},
+  },
+  {
+    path: '/note/:id',
+    name: 'Note',
+    component: Note,
+    meta: {requiresAuth: true},
+    props: true,
+  },
+  {
+    path: '/note/:id',
+    name: 'EditNote',
+    component: EditNote,
+    meta: {requiresAuth: true},
+    props: true,
   }
 ]
 
